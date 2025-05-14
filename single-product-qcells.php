0bddacdaee8a0a94bedb647fb7f516a5b40e99fe
Mori <mori@felixjapan.net>
2025-05-14 13:08:03 +0900

    feat: 製品詳細ページ(Qcells) 各項目をカスタムフィールドで表示されるように変更

--- single-product-qcells.php
+++ single-product-qcells.php
@@ -175,222 +175,22 @@ $img_path = get_stylesheet_directory_uri() . "/images";
         <div class="wrapper">
           <h2 class="sec-ttl" id="feature">特長</h2>
         </div>
-        <ul class="feature_list">
-          <li class="item">
-            <div class="wrapper">
-              <div class="feature-ttl">
-                <span class="no">1</span>
-                <h3 class="txt">独自のQ.ANTUM NEOテクノロジー</h3>
-              </div>
-              <p class="lead">世界で認められた最先端技術で高出力・高効率</p>
-            </div>
-            <div class="primary">
-              <div class="primary-wrapper">
-                <div class="left">
-                  <div class="point">
-                    <h4 class="ttl">Q.ANTUM NEOセルの効率</h4>
-                    <div class="imgc">
-                      <img src="<?= $img_path ?>/single-product/feature-1-1.jpg" alt="">
-                    </div>
-                  </div>
-                  <div class="point">
-                    <h4 class="ttl">Q.ANTUM NEOセルの効率</h4>
-                    <div class="imgc">
-                      <img src="<?= $img_path ?>/single-product/feature-1-2.jpg" alt="">
-                    </div>
-                  </div>
-                </div>
-                <div class="right">
-                  <div class="right-wrap">
-                    <p class="outline">
-                      Q.TRON M-G2.4+は<br>Qセルズ独自の最先端技術の<br>Q.ANTUM NEOテクノロジーを採用された高効率N型モジュールです。
-                    </p>
-                    <ul class="right_point_list">
-                      <li class="item">高出力と高効率を兼ね備えたTOPCon構造に基づいたQ.ANTUM NEOテクノロジーで、22.5％まで変換効率を向上</li>
-                      <li class="item">N型セルでより多くの電子が裏面の電極に到達させ、さらに高出力・高効率を実現</li>
-                      <li class="item">優れた温度係数で、表面温度上昇に伴う効率の低下を改善</li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li class="item">
-            <div class="wrapper">
-              <div class="feature-ttl">
-                <span class="no">2</span>
-                <h3 class="txt">Qセルズならではの厳格な品質管理</h3>
-              </div>
-              <p class="lead">世界最高レベルの「品質検証認証」QCPV</p>
-            </div>
-            <div class="primary">
-              <div class="primary-wrapper">
-                <div class="left">
-                  <div class="point">
-                    <h4 class="ttl">過酷な環境ストレステスト</h4>
-                    <div class="imgc">
-                      <img src="<?= $img_path ?>/single-product/feature-2-1.jpg" alt="">
-                    </div>
-                  </div>
-                  <div class="point">
-                    <h4 class="ttl">生産モニタリング</h4>
-                    <div class="imgc">
-                      <img src="<?= $img_path ?>/single-product/feature-2-2.jpg" alt="">
-                    </div>
-                  </div>
-                  <div class="point">
-                    <h4 class="ttl">品質テスト一例</h4>
-                    <div class="imgc">
-                      <img src="<?= $img_path ?>/single-product/feature-2-3.jpg" alt="">
-                    </div>
-                  </div>
-                </div>
-                <div class="right">
-                  <div class="right-wrap">
-                    <p class="outline">
-                      世界的な太陽電池モジュール（太陽光パネル）の認証機関である、ドイツのTÜV Rheinland（テュフラインランド）が設計した品質設定プログラムで、優れた性能と信頼性を認定されました。
-                    </p>
-                    <ul class="right_point_list">
-                      <li class="item">IEC規格※の最大3倍に及ぶ過酷な品質テストを実施、外部環境の変化にも高い発電量を維持</li>
-                      <li class="item">生産過程でTÜV Rheinlandの専門家がランダムサンプリング検査・モニタリング</li>
-                      <li class="item">高度な試験方法による部材の試験と総合的なサプライヤー管理</li>
-                    </ul>
-                    <p class="note">IEC規格：国際電気標準会議が定めた電気及び電子技術分野の国際標準規格</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li class="item">
-            <div class="wrapper">
-              <div class="feature-ttl">
-                <span class="no">3</span>
-                <h3 class="txt">25年長期保証でOOO円の経済効果</h3>
-              </div>
-              <p class="lead">長期保証による、25年間の経済的なメリットが期待できます。</p>
-            </div>
-            <div class="primary">
-              <div class="primary-wrapper">
-                <div class="left">
-                  <div class="point">
-                    <h4 class="ttl">出力性能</h4>
-                    <div class="imgc">
-                      <img src="<?= $img_path ?>/single-product/feature-3-1.jpg" alt="">
-                    </div>
-                  </div>
-                </div>
-                <div class="right">
-                  <div class="right-wrap">
-                    <p class="outline">
-                      長期間の保証でもしものときも安心できるだけでなく、長期保証から25年間発生する約4,710kWh（後程経済メリットを感じられる数字で計算）のお得もご期待いただけます。
-                    </p>
-                    <p class="note">注釈テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
+        <?= the_field('sec_feat') ?>
       </section>
       <!-- //feature -->
 
       <section class="warranty" data-target="warranty">
         <div class="wrapper">
           <h2 class="sec-ttl" id="warranty">保証</h2>
-          <ul class="warranty_list">
-            <li class="item item-1">
-              <div class="detail">
-                <div class="imgc">
-                  <img src="<?= $img_path ?>/single-product/warranty-item-1.png" alt="25年製品保証">
-                </div>
-                <div class="heading">出力保証</div>
-                <p class="txt">太陽電池モジュール（太陽光パネル）の出力性能を、25年もの長期間保証いたします。</p>
-              </div>
-              <div class="graph">
-                <img src="<?= $img_path ?>/single-product/warranty-graph.jpg" alt="">
-              </div>
-            </li>
-            <li class="item item-2">
-              <div class="detail">
-                <div class="imgc">
-                  <img src="<?= $img_path ?>/single-product/warranty-item-2.png" alt="25年製品保証">
-                </div>
-                <div class="heading">製品保証</div>
-                <p class="txt">Q.TRONシリーズは高い品質管理とともに、25年の製品保証を提供して長く安心してご使用いただけます。</p>
-              </div>
-              <p class="note">※太陽電池モジュールのみ販売の場合。システムとして販売の場合と保証開始日と保証規定が異なります。</p>
-            </li>
-          </ul>
+          <?= the_field('sec_warranty') ?>
         </div>
       </section>
       <!-- //warranty -->
 
       <section class="guide" data-target="guide">
         <div class="wrapper">
-          <h2 class="sec-ttl" id="guide"><?= the_field('guide_title') ?></h2>
-          <ul class="flow">
-            <li class="step">
-              <p class="no">STEP1</p>
-              <h3 class="step-ttl"><?= the_field('step1_title') ?></h3>
-              <div class="imgc">
-                <img src="<?= get_field('step1_img')['url'] ?>" alt="">
-              </div>
-              <div class="accordion typeB">
-                <div class="accordion-btn">
-                  <p class="txt">詳細を見る</p>
-                  <span class="icon"></span>
-                </div>
-                <div class="accordion-content">
-                  <p class="txt">
-                    <?= the_field('step1_detail') ?>
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li class="step">
-              <p class="no">STEP2</p>
-              <h3 class="step-ttl"><?= the_field('step2_title') ?></h3>
-              <div class="imgc">
-                <img src="<?= get_field('step2_img')['url'] ?>" alt="">
-              </div>
-              <div class="accordion typeB">
-                <div class="accordion-btn">
-                  <p class="txt">詳細を見る</p>
-                  <span class="icon"></span>
-                </div>
-                <div class="accordion-content">
-                  <p class="txt">
-                    <?= the_field('step2_detail') ?>
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li class="step">
-              <p class="no">STEP3</p>
-              <h3 class="step-ttl"><?= the_field('step3_title') ?></h3>
-              <div class="imgc">
-                <img src="<?= get_field('step3_img')['url'] ?>" alt="">
-              </div>
-              <div class="accordion typeB">
-                <div class="accordion-btn">
-                  <p class="txt">詳細を見る</p>
-                  <span class="icon"></span>
-                </div>
-                <div class="accordion-content">
-                  <p class="txt">
-                    <?= the_field('step3_detail') ?>
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li class="step">
-              <p class="no">STEP4</p>
-              <h3 class="step-ttl"><?= the_field('step4_title') ?></h3>
-              <div class="imgc">
-                <img src="<?= get_field('step4_img')['url'] ?>" alt="">
-              </div>
-            </li>
-          </ul>
+          <h2 class="sec-ttl" id="guide">安心導入ガイド</h2>
+          <?= the_field('sec_guide'); ?>
         </div>
       </section>
       <!-- //guide -->
@@ -398,21 +198,7 @@ $img_path = get_stylesheet_directory_uri() . "/images";
       <section class="support" data-target="support">
         <div class="wrapper">
           <h2 class="sec-ttl" id="support">ご検討から設置後のアフターまでも徹底サポートいたします。</h2>
-          <div class="support_list">
-            <a href="<?= get_field('left_link')['url'] ?>" class="item">
-              <div class="imgc">
-                <img src="<?= $img_path ?>/single-product/support_icon_1.svg" alt="">
-              </div>
-              <p class="txt txt-faq"><?= get_field('left_link')['title'] ?></p>
-            </a>
-            <span class="border"></span>
-            <a href="<?= get_field('right_link')['url'] ?>" class="item">
-              <div class="imgc">
-                <img src="<?= $img_path ?>/single-product/support_icon_2.svg" alt="">
-              </div>
-              <p class="txt txt-after"><?= get_field('right_link')['title'] ?></p>
-            </a>
-          </div>
+          <?= the_field('sec_support'); ?>
         </div>
       </section>
       <!-- //support -->
@@ -440,7 +226,7 @@ $img_path = get_stylesheet_directory_uri() . "/images";
                 array(
                   'taxonomy' => 'product-cat',
                   'field'    => 'slug',
-                  'terms'    => 'q-cells',
+                  'terms'    => array('q-cells'),
                 ),
               ),
             );
@@ -751,7 +537,7 @@ $img_path = get_stylesheet_directory_uri() . "/images";
           <span class="icon"></span>
         </div>
       </div>
-      <a href="#" class="banner">
+      <a href="<?= the_field('compare_link'); ?>" class="banner">
         <img src="<?= $img_path ?>/single-product/qcells_banner.png" alt="Qcells太陽光パネル製品一覧ページへ">
       </a>
     </div>
