e2bfcb69f25232f861b76e582a3dbe4d0143ddc2
Mori <mori@felixjapan.net>
2025-05-23 17:02:22 +0900

    feat: 「お問い合わせ・カタログ」ページ（TOP）を作成　※page-contact-catalog.phpから使用テンプレートを変更

---
+++ page-service-support.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: お問い合わせ・カタログTOP
+*/
+get_header();
+?>
+
+<main>
+  <div class="wrap">
+    <?php custom_breadcrumb(); ?>
+  </div>
+  <section>
+    <div class="wrap">
+      <h1>お問い合わせ・カタログ　TOPページ</h1>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
