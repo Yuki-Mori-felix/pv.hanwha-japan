613f40f3bab1ac0f7be837911bfcbc40fd19b58c
Mori <mori@felixjapan.net>
2025-05-29 16:25:16 +0900

    feat: 製品一覧ページ(Re.RISE)、製品比較機能を実装

--- page-product-cat-re-rise.php
+++ page-product-cat-re-rise.php
@@ -6,7 +6,7 @@ get_header();
 $img_path = get_stylesheet_directory_uri() . "/images";
 ?>
 <!-- 製品詳細 ターム(Re.RISE) 一覧ページ -->
-<main class="taxonomy-product-cat re-rise">
+<main class="taxonomy-product-cat re-rise single-products rerise">
   <section class="mv mv-re-rise">
     <div class="container">
       <div class="ttlc">
@@ -131,6 +131,303 @@ $img_path = get_stylesheet_directory_uri() . "/images";
     </div>
   </section>
   <!-- // LINEUP -->
+
+  <section class="compare" data-target="compare">
+    <div class="wrapper">
+      <h2 class="sec-ttl" id="compare">製品を比較する</h2>
+      <div class="product top">
+        <?php
+        // **指定した2つの投稿IDをデフォルトとの比較製品として設定**
+        $default_product_id = get_field('default_product'); // 任意の投稿IDを設定
+        $default_compare_id = get_field('default_compare_product'); // 任意の比較対象の投稿IDを設定
+
+        // 最新の投稿のリストを取得（セレクトボックス用）
+        $args = array(
+          'post_type'      => 'product',
+          'posts_per_page' => -1,
+          'tax_query'      => array(
+            array(
+              'taxonomy' => 'product-cat',
+              'field'    => 'slug',
+              'terms'    => array('re-rise'), // ターム値を変更可能
+            ),
+          ),
+        );
+
+        $query = new WP_Query($args);
+        $post_ids = [];
+
+        if ($query->have_posts()) {
+          while ($query->have_posts()) {
+            $query->the_post();
+            $post_ids[get_the_ID()] = get_the_title(); // 投稿IDをキーにタイトルを保存
+          }
+          wp_reset_postdata();
+        }
+
+        // **投稿IDが未設定の場合は、最新の投稿を使用**
+        if (!$default_product_id || !$default_compare_id) {
+          $latest_posts = array_keys($post_ids); // 投稿IDを取得
+
+          $default_product_id = $latest_posts[0] ?? null; // 最新の投稿
+          $default_compare_id = $latest_posts[1] ?? null; // 2番目の投稿
+        }
+
+        // 投稿IDに基づいて画像URLを取得
+        $product_image_url = esc_url(get_field('image1', $default_product_id)['url'] ?? '');
+        $compare_image_url = esc_url(get_field('image1', $default_compare_id)['url'] ?? '');
+        ?>
+
+        <!-- メインの製品 -->
+        <div class="item">
+          <div class="imgc">
+            <img id="product-image" src="<?= $product_image_url; ?>" alt="">
+          </div>
+          <div class="name">
+            <select id="product-select" class="select-product-name">
+              <?php foreach ($post_ids as $post_id => $title) : ?>
+                <option value="<?= $post_id; ?>" <?= ($post_id === $default_product_id) ? 'selected' : ''; ?>>
+                  <?= esc_html($title); ?>
+                </option>
+              <?php endforeach; ?>
+            </select>
+          </div>
+          <a id="product-more-link" href="<?= esc_url(get_permalink($default_product_id)); ?>" class="more">詳しく見る</a>
+        </div>
+
+        <!-- 比較対象の製品 -->
+        <div class="item">
+          <div class="imgc">
+            <img id="compare-product-image" src="<?= $compare_image_url; ?>" alt="">
+          </div>
+          <div class="name">
+            <select id="compare-product-select" class="select-product-name">
+              <?php foreach ($post_ids as $post_id => $title) : ?>
+                <option value="<?= $post_id; ?>" <?= ($post_id === $default_compare_id) ? 'selected' : ''; ?>>
+                  <?= esc_html($title); ?>
+                </option>
+              <?php endforeach; ?>
+            </select>
+          </div>
+          <a id="compare-product-more-link" href="<?= esc_url(get_permalink($default_compare_id)); ?>" class="more">詳しく見る</a>
+        </div>
+
+      </div>
+      <div class="table table-visible">
+        <div class="row row-t">
+          <div class="heading">
+            <div class="imgc">
+              <img src="<?= $img_path ?>/single-product/compare_icon_1.svg" alt="">
+            </div>
+            <p class="txt">公称最大出力<span class="sm">（+5W / -0W）</span></p>
+          </div>
+          <div class="product">
+            <div class="item">
+              <p class="data-txt" id="product-detail-1"><?= wp_kses_post(get_field('product_detail_1')); ?></p>
+            </div>
+            <div class="item">
+              <p class="data-txt" id="compare-product-detail-1"><?= wp_kses_post(get_field('product_detail_1', $default_compare_post_id)); ?></p>
+            </div>
+          </div>
+        </div>
+        <div class="row row-t">
+          <div class="heading">
+            <div class="imgc">
+              <img src="<?= $img_path ?>/single-product/compare_icon_2.svg" alt="">
+            </div>
+            <p class="txt">最大変換効率</p>
+          </div>
+          <div class="product">
+            <div class="item">
+              <p class="data-txt" id="product-detail-2"><?= wp_kses_post(get_field('product_detail_2')); ?></p>
+            </div>
+            <div class="item">
+              <p class="data-txt" id="compare-product-detail-2"><?= wp_kses_post(get_field('product_detail_2', $default_compare_post_id)); ?></p>
+            </div>
+          </div>
+        </div>
+        <div class="row row-t">
+          <div class="heading">
+            <div class="imgc">
+              <img src="<?= $img_path ?>/single-product/compare_icon_3.svg" alt="">
+            </div>
+            <p class="txt">外形寸法<span class="sm">（横×高さ×奥行）</span></p>
+          </div>
+          <div class="product">
+            <div class="item">
+              <p class="data-txt" id="product-detail-3"><?= wp_kses_post(get_field('product_detail_3')); ?></p>
+            </div>
+            <div class="item">
+              <p class="data-txt" id="compare-product-detail-3"><?= wp_kses_post(get_field('product_detail_3', $default_compare_post_id)); ?></p>
+            </div>
+          </div>
+        </div>
+        <div class="row row-t">
+          <div class="heading">
+            <div class="imgc">
+              <img src="<?= $img_path ?>/single-product/compare_icon_4.svg" alt="">
+            </div>
+            <p class="txt">保証</p>
+          </div>
+          <div class="product">
+            <div class="item">
+              <p class="data-txt" id="product-detail-12"><?= wp_kses_post(get_field('product_detail_12')); ?></p>
+            </div>
+            <div class="item">
+              <p class="data-txt" id="compare-product-detail-12"><?= wp_kses_post(get_field('product_detail_12', $default_compare_post_id)); ?></p>
+            </div>
+          </div>
+        </div>
+        <div class="row row-t">
+          <div class="heading">
+            <div class="imgc">
+              <img src="<?= $img_path ?>/single-product/compare_icon_5.svg" alt="">
+            </div>
+            <p class="txt">メーカー希望小売価格<span class="sm">（税込）</span></p>
+          </div>
+          <div class="product">
+            <div class="item">
+              <p class="data-txt" id="product-detail-14"><?= wp_kses_post(get_field('product_detail_14')); ?></p>
+            </div>
+            <div class="item">
+              <p class="data-txt" id="compare-product-detail-14"><?= wp_kses_post(get_field('product_detail_14', $default_compare_post_id)); ?></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="accordion typeA">
+        <div class="accordion-content">
+          <div class="table">
+            <div class="row row-acc">
+              <div class="heading">
+                <p class="txt-acc">公称短絡電流（Isc）</p>
+              </div>
+              <div class="product">
+                <div class="item">
+                  <p class="data-txt" id="product-detail-5"><?= wp_kses_post(get_field('product_detail_5')); ?></p>
+                </div>
+                <div class="item">
+                  <p class="data-txt" id="compare-product-detail-5"><?= wp_kses_post(get_field('product_detail_5', $default_compare_post_id)); ?></p>
+                </div>
+              </div>
+            </div>
+            <div class="row row-acc">
+              <div class="heading">
+                <p class="txt-acc">公称開放電圧（Voc）</p>
+              </div>
+              <div class="product">
+                <div class="item">
+                  <p class="data-txt" id="product-detail-6"><?= wp_kses_post(get_field('product_detail_6')); ?></p>
+                </div>
+                <div class="item">
+                  <p class="data-txt" id="compare-product-detail-6"><?= wp_kses_post(get_field('product_detail_6', $default_compare_post_id)); ?></p>
+                </div>
+              </div>
+            </div>
+            <div class="row row-acc">
+              <div class="heading">
+                <p class="txt-acc">公称最大出力動作電流（Imp）</p>
+              </div>
+              <div class="product">
+                <div class="item">
+                  <p class="data-txt" id="product-detail-7"><?= wp_kses_post(get_field('product_detail_7')); ?></p>
+                </div>
+                <div class="item">
+                  <p class="data-txt" id="compare-product-detail-7"><?= wp_kses_post(get_field('product_detail_7', $default_compare_post_id)); ?></p>
+                </div>
+              </div>
+            </div>
+            <div class="row row-acc">
+              <div class="heading">
+                <p class="txt-acc">公称最大出力動作電圧（Vmp）</p>
+              </div>
+              <div class="product">
+                <div class="item">
+                  <p class="data-txt" id="product-detail-8"><?= wp_kses_post(get_field('product_detail_8')); ?></p>
+                </div>
+                <div class="item">
+                  <p class="data-txt" id="compare-product-detail-8"><?= wp_kses_post(get_field('product_detail_8', $default_compare_post_id)); ?></p>
+                </div>
+              </div>
+            </div>
+            <div class="row row-acc">
+              <div class="heading">
+                <p class="txt-acc">質量</p>
+              </div>
+              <div class="product">
+                <div class="item">
+                  <p class="data-txt" id="product-detail-4"><?= wp_kses_post(get_field('product_detail_4')); ?></p>
+                </div>
+                <div class="item">
+                  <p class="data-txt" id="compare-product-detail-4"><?= wp_kses_post(get_field('product_detail_4', $default_compare_post_id)); ?></p>
+                </div>
+              </div>
+            </div>
+            <div class="row row-acc">
+              <div class="heading">
+                <p class="txt-acc">フレーム材料</p>
+              </div>
+              <div class="product">
+                <div class="item">
+                  <p class="data-txt" id="product-detail-9"><?= wp_kses_post(get_field('product_detail_9')); ?></p>
+                </div>
+                <div class="item">
+                  <p class="data-txt" id="compare-product-detail-9"><?= wp_kses_post(get_field('product_detail_9', $default_compare_post_id)); ?></p>
+                </div>
+              </div>
+            </div>
+            <div class="row row-acc">
+              <div class="heading">
+                <p class="txt-acc">セル</p>
+              </div>
+              <div class="product">
+                <div class="item">
+                  <p class="data-txt" id="product-detail-10"><?= wp_kses_post(get_field('product_detail_10')); ?></p>
+                </div>
+                <div class="item">
+                  <p class="data-txt" id="compare-product-detail-10"><?= wp_kses_post(get_field('product_detail_10', $default_compare_post_id)); ?></p>
+                </div>
+              </div>
+            </div>
+            <div class="row row-acc">
+              <div class="heading">
+                <p class="txt-acc">端子ボックス防水特性</p>
+              </div>
+              <div class="product">
+                <div class="item">
+                  <p class="data-txt" id="product-detail-11"><?= wp_kses_post(get_field('product_detail_11')); ?></p>
+                </div>
+                <div class="item">
+                  <p class="data-txt" id="compare-product-detail-11"><?= wp_kses_post(get_field('product_detail_11', $default_compare_post_id)); ?></p>
+                </div>
+              </div>
+            </div>
+            <div class="row row-acc">
+              <div class="heading">
+                <p class="txt-acc">最大圧風荷重 / 最大表面荷重</p>
+              </div>
+              <div class="product">
+                <div class="item">
+                  <p class="data-txt" id="product-detail-13"><?= wp_kses_post(get_field('product_detail_13')); ?></p>
+                </div>
+                <div class="item">
+                  <p class="data-txt" id="compare-product-detail-13"><?= wp_kses_post(get_field('product_detail_13', $default_compare_post_id)); ?></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-btn">
+          <p class="txt">すべての仕様を比較</p>
+          <span class="icon"></span>
+        </div>
+      </div>
+      <a href="<?= the_field('compare_link'); ?>" class="banner">
+        <img src="<?= $img_path ?>/single-product/Re.RISE_Product details_banner.png" alt="Re.RISE太陽光パネル製品一覧ページへ">
+      </a>
+    </div>
+  </section>
+  <!-- //compare -->
 </main>
 <!-- // TAXONOMY_PRODUCTS_CAT RE_RISE -->
 <?=get_footer();?>
