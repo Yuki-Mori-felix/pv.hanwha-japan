292240d12ddc5a9ef81f1617dcfe944191e204f9
Mori <mori@felixjapan.net>
2025-06-03 17:55:50 +0900

    chore: 固定ページ「ハンファジャパンのpv」下層ページにunder-pv.cssを読み込み

--- header.php
+++ header.php
@@ -21,6 +21,8 @@
 		<?php endif; ?>
   <?php elseif (is_page('pv')) : ?>
     <link rel='stylesheet' href='<?= get_stylesheet_directory_uri() ?>/css/pv.css'>
+  <?php elseif (is_page('location-details') || is_page('achievement-details') || is_page('award-details') || is_page('support-details') || is_page('technology-details') || is_page('transition-details') ) : ?>
+    <link rel='stylesheet' href='<?= get_stylesheet_directory_uri() ?>/css/under-pv.css'>
   <?php elseif (is_page('product-list')) : ?>
     <link rel='stylesheet' href='<?= get_stylesheet_directory_uri() ?>/css/product-list.css'>
   <?php elseif (is_singular('product')) : ?>
