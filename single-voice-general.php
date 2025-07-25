e0257fd99d10370716f49abf597ef8ae69110d39
Mori <mori@felixjapan.net>
2025-07-25 11:15:23 +0900

    fix: お客様インタビュー詳細ページ「一般」「販売店」、ハンファ修正依頼対応

--- single-voice-general.php
+++ single-voice-general.php
@@ -102,8 +102,8 @@ Template Post Type: voice
 			<div class="sv-wrap">
 				<h2 class="sec-ttl">お問い合わせ・お見積り依頼はこちらから</h2>
 				<div class="contact-links">
-					<a href="" class="contact-link link-1">お問い合わせ</a>
-					<a href="" class="contact-link link-2">お見積り依頼</a>
+					<a href="https://pv.hanwha-japan.com/service-support/contact/" class="contact-link link-1">お問い合わせ</a>
+					<a href="https://pv.hanwha-japan.com/service-support/contact-quotation/" class="contact-link link-2">お見積り依頼</a>
 				</div>
 			</div>
 		</section>
@@ -111,19 +111,103 @@ Template Post Type: voice
 
 		<section class="back-to-archive">
 			<div class="sv-wrap">
-				<a href="" class="to-archive-btn">
+				<a href="https://pv.hanwha-japan.com/voice/interview/" class="to-archive-btn">
 					<span class="to-archive-txt">インタビューの一覧へ戻る</span>
 					<span class="to-archive-icon"></span></a>
 			</div>
 		</section>
 		<!-- back-to-archive -->
 
-		<?= the_field('sec_guide', $post_id, false); ?>
+		<!-- <?= the_field('sec_guide', $post_id, false); ?> -->
+    <section class="guide" data-target="guide">
+      <div class="wrapper">
+        <h2 class="sec-ttl" id="guide">安心導入ガイド</h2>
+        <ul class="flow">
+          <li class="step">
+            <p class="no">STEP1</p>
+            <h3 class="step-ttl">サイトでお見積り依頼</h3>
+            <div class="imgc">
+              <img decoding="async" src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/guide-step-1.png" alt="">
+            </div>
+            <div class="accordion typeB">
+              <div class="accordion-btn">
+                <p class="txt">詳細を見る</p>
+                <p> <span class="icon"></span>
+                </p>
+              </div>
+              <div class="accordion-content">
+                <p class="txt">
+                  ・ウェブサイトのお見積り依頼フォームをご記入ください。<br />
+                  ・担当者から確認後ご連絡します。<br />
+                  その後当社から販売店をご紹介します。
+                </p>
+              </div>
+            </div>
+          </li>
+          <li class="step">
+            <p class="no">STEP2</p>
+            <h3 class="step-ttl">ヒアリング・ご提案</h3>
+            <div class="imgc">
+              <img decoding="async" src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/guide-step-2.png" alt="">
+            </div>
+            <div class="accordion typeB">
+              <div class="accordion-btn">
+                <p class="txt">詳細を見る</p>
+                <p> <span class="icon"></span>
+                </p>
+              </div>
+              <div class="accordion-content">
+                <p class="txt">
+                  ・ご紹介した販売店から具体的な状況（例：電気代のご使用状況）やご希望などヒアリングいたします。<br />
+                  ・ヒアリングの上、最適な内容をご提案いたします。
+                </p>
+              </div>
+            </div>
+          </li>
+          <li class="step">
+            <p class="no">STEP3</p>
+            <h3 class="step-ttl">ご契約・施工</h3>
+            <div class="imgc">
+              <img decoding="async" src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/guide-step-3.png" alt="">
+            </div>
+            <div class="accordion typeB">
+              <div class="accordion-btn">
+                <p class="txt">詳細を見る</p>
+                <p> <span class="icon"></span>
+                </p>
+              </div>
+              <div class="accordion-content">
+                <p class="txt">
+                  ・ご契約の後、現地調査、施工を行います。（販売店によりご契約、現地調査は前後する場合がございます）
+                </p>
+              </div>
+            </div>
+          </li>
+          <li class="step">
+            <p class="no">STEP4</p>
+            <h3 class="step-ttl">設置完了</h3>
+            <div class="imgc">
+              <img decoding="async" src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/guide-step-4.png" alt="">
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
 		<!-- //guide -->
 
-		<?= the_field('sec_support', $post_id, false); ?>
+		<!-- <?= the_field('sec_support', $post_id, false); ?> -->
+    <section class="support" data-target="support">
+      <div class="wrapper">
+        <h2 class="sec-ttl" id="support">丁寧な導入サポート、充実な保証制度でご安心いただけます。</h2>
+        <div class="support_list">
+          <p class="item"><a href="https://pv.hanwha-japan.com/service-support/contact/"><span class="imgc"><img src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/support_icon_1.svg" alt=""></span><span class="txt txt-faq">導入・製品に関するご質問</span></a></p>
+          <p class="border">
+          <p class="item"><a href="https://pv.hanwha-japan.com/service-support/after-follow/"><span class="imgc"><img src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/support_icon_2.svg" alt=""></span><span class="txt txt-after">充実した保証</span></a></p>
+        </div>
+      </div>
+    </section>
 		<!-- //support -->
-		 
+
 	</main>
 	<!-- #main -->
 
--- single-voice.php
+++ single-voice.php
@@ -41,6 +41,12 @@ get_header(); ?>
                     <div class="status-text"><?= get_field('solar_panel'); ?></div>
                   </li>
                 <?php endif; ?>
+                <?php if (get_field('installed_capacity')): ?>
+                  <li class="status-item">
+                    <div class="status-title">設置容量</div>
+                    <div class="status-text"><?= get_field('installed_capacity'); ?></div>
+                  </li>
+                <?php endif; ?>
                 <?php if (get_field('storage_battery')): ?>
                   <li class="status-item">
                     <div class="status-title">蓄電池</div>
@@ -94,6 +100,131 @@ get_header(); ?>
       </section>
       <!-- problem -->
 
+      <!-- <section class="why">
+        <div class="common-voice-title">
+          <h3 class="cvt-txt">導入を決めた<span class="cvt-bold">きっかけ</span></h3>
+        </div>
+        <div class="common-voice-content">
+          <div class="cvc-item">
+            <div class="cvc-wrap">
+              <div class="cvc-textarea">
+                <h4 class="cvc-title">震災後に考えた<br>「自分らしいエネルギー活用」。</h4>
+                <p class="cvc-text">設置しようと思った理由は二つあります。一つは東日本大震災を受けて、地球環境への貢献を真剣に考えるようになったこと。もう一つは、電力会社の再生エネルギー発電賦課金の支払い義務を通して、自ら出来ることはなにかと考えたことです。<br><br>屋根が南向きで、設置環境としては恵まれていたので、見積もりを取ってから設置までは早かったです。環境先進国であるドイツのノウハウを持つＱセルズにはとても期待していました。それは単に売電額に対してだけではなく、今あるライフラインの契約内容を見直すチャンスにもなるという意味です。<br><br>具体的には夏や冬など、シーズンにより使用する電気量も発電量も異なり、それに合わせて電力会社とのアンペア契約を変更します。特に東北の冬は雪がよく雪が降りますが、うちでは蓄熱暖房も導入して、暖房に消費される電力を節約しています。<br><br>太陽光発電システムの設置をきっかけに、多角的にエネルギーをいかにうまく使うか、という視野を持つようになりました。</p>
+              </div>
+              <div class="cvc-img-list">
+                <div class="cvc-img">
+                  <img src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/view-3-A-1.jpg" alt="" width="600" height="550" class="alignnone size-full wp-image-1406" />
+                  <p class="cvc-figure">ダミーテキスト</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="common-voice-content">
+          <div class="cvc-item">
+            <div class="cvc-wrap">
+              <div class="cvc-textarea">
+                <h4 class="cvc-title">震災後に考えた<br>「自分らしいエネルギー活用」。</h4>
+                <p class="cvc-text">設置しようと思った理由は二つあります。一つは東日本大震災を受けて、地球環境への貢献を真剣に考えるようになったこと。もう一つは、電力会社の再生エネルギー発電賦課金の支払い義務を通して、自ら出来ることはなにかと考えたことです。<br><br>屋根が南向きで、設置環境としては恵まれていたので、見積もりを取ってから設置までは早かったです。環境先進国であるドイツのノウハウを持つＱセルズにはとても期待していました。それは単に売電額に対してだけではなく、今あるライフラインの契約内容を見直すチャンスにもなるという意味です。<br><br>具体的には夏や冬など、シーズンにより使用する電気量も発電量も異なり、それに合わせて電力会社とのアンペア契約を変更します。特に東北の冬は雪がよく雪が降りますが、うちでは蓄熱暖房も導入して、暖房に消費される電力を節約しています。<br><br>太陽光発電システムの設置をきっかけに、多角的にエネルギーをいかにうまく使うか、という視野を持つようになりました。</p>
+              </div>
+              <div class="cvc-img-list">
+                <div class="cvc-img">
+                  <img src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/view-3-A-1.jpg" alt="" width="600" height="550" class="alignnone size-full wp-image-1406" />
+                  <p class="cvc-figure">ダミーテキスト</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section> -->
+      <!-- why -->
+
+      <!-- <section class="changes">
+        <div class="common-voice-title">
+          <h3 class="cvt-txt">購入後の<span class="cvt-bold">変化</span></h3>
+        </div>
+        <div class="common-voice-content">
+          <div class="cvc-item">
+            <div class="cvc-wrap">
+              <div class="cvc-textarea">
+                <h4 class="cvc-title">自ら電力を生み出すということが<br>変える価値観</h4>
+                <p class="cvc-text">毎日の発電量をモニターで確認すること、売電を実感することで、節電の意識が非常に高くなりました。<br><br>家ではある意味“プロジェクト”になっています。私の家はオール家電で、例えば冬場にお風呂の湯を温めるにも費用が大きくかかります。節電をプロジェクトとして家族で意識共有し、その費用を実感することで、無駄を無くすように声をかけあったりします。</p>
+              </div>
+              <div class="cvc-img-list">
+                <div class="cvc-img">
+                  <img src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/view-3-B-1.png" alt="" width="586" height="303" class="alignnone size-full wp-image-1405" />
+                  <p class="cvc-figure">ダミーテキスト</p>
+                </div>
+                <div class="cvc-img">
+                  <img src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/view-3-B-2.png" alt="" width="592" height="303" class="alignnone size-full wp-image-1404" />
+                  <p class="cvc-figure">ダミーテキスト</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="common-voice-content">
+          <div class="cvc-item">
+            <div class="cvc-wrap">
+              <div class="cvc-textarea">
+                <h4 class="cvc-title">自ら電力を生み出すということが<br>変える価値観</h4>
+                <p class="cvc-text">毎日の発電量をモニターで確認すること、売電を実感することで、節電の意識が非常に高くなりました。<br><br>家ではある意味“プロジェクト”になっています。私の家はオール家電で、例えば冬場にお風呂の湯を温めるにも費用が大きくかかります。節電をプロジェクトとして家族で意識共有し、その費用を実感することで、無駄を無くすように声をかけあったりします。</p>
+              </div>
+              <div class="cvc-img-list">
+                <div class="cvc-img">
+                  <img src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/view-3-B-1.png" alt="" width="586" height="303" class="alignnone size-full wp-image-1405" />
+                  <p class="cvc-figure">ダミーテキスト</p>
+                </div>
+                <div class="cvc-img">
+                  <img src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/view-3-B-2.png" alt="" width="592" height="303" class="alignnone size-full wp-image-1404" />
+                  <p class="cvc-figure">ダミーテキスト</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section> -->
+      <!-- changes -->
+
+
+      <!-- <section class="message">
+        <div class="common-voice-title">
+          <h3 class="cvt-txt">ご検討中の方に<span class="cvt-bold">伝えたいこと</span></h3>
+        </div>
+        <div class="common-voice-content">
+          <div class="cvc-item">
+            <div class="cvc-wrap">
+              <div class="cvc-textarea">
+                <h4 class="cvc-title">何が起こるかがわからないからこそ、<br>安心の保証</h4>
+                <p class="cvc-text">昨今、地震だけではなく、天変地異が起こっていて、あまりに予測不能なことが多くなっています。その中で、出力保証とシステム保証が長期にわたっているのは安心ですね。<br><br>ローンを払い終わるころに、ちょうど子供たちが日本を背負う時代になると思っているのですが、代が変わっても保証が継続されているのは親としても大きな安心です。</p>
+              </div>
+              <div class="cvc-img-list">
+                <div class="cvc-img">
+                  <img src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/view-3-A-1.jpg" alt="" width="525" height="276" class="alignnone size-large wp-image-1403" />
+                  <p class="cvc-figure">ダミーテキスト</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="common-voice-content">
+          <div class="cvc-item">
+            <div class="cvc-wrap">
+              <div class="cvc-textarea">
+                <h4 class="cvc-title">何が起こるかがわからないからこそ、<br>安心の保証</h4>
+                <p class="cvc-text">昨今、地震だけではなく、天変地異が起こっていて、あまりに予測不能なことが多くなっています。その中で、出力保証とシステム保証が長期にわたっているのは安心ですね。<br><br>ローンを払い終わるころに、ちょうど子供たちが日本を背負う時代になると思っているのですが、代が変わっても保証が継続されているのは親としても大きな安心です。</p>
+              </div>
+              <div class="cvc-img-list">
+                <div class="cvc-img">
+                  <img src="https://pv.hanwha-japan.com/wp-content/uploads/2025/05/view-3-A-1.jpg" alt="" width="525" height="276" class="alignnone size-large wp-image-1403" />
+                  <p class="cvc-figure">ダミーテキスト</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section> -->
+      <!-- message -->
       <?= get_field('main_content', false, false); ?>
       <!-- WHY, CHANGES, MESSAGE -->
 
