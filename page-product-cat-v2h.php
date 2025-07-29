8cb43651448cfb5973c17d3cc89d7bbf0bd8f5e3
Mori <mori@felixjapan.net>
2025-07-29 11:50:07 +0900

    fix: V2H修正

--- page-product-cat-v2h.php
+++ page-product-cat-v2h.php
@@ -35,10 +35,10 @@ $img_path = get_stylesheet_directory_uri() . "/images";
   </section>
   <!-- // INTRO -->
 
-  <?= get_field('content_1', false, false); ?>
+  <?= get_field('content_1'); ?>
   <!-- // POWER_STATION -->
 
-  <?= get_field('content_2', false, false); ?>
+  <?= get_field('content_2'); ?>
   <!-- // TRIBRID -->
 </main>
 <?= get_footer(); ?>
