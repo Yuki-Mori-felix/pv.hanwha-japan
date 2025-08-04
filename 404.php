b80ff45d5e396e617d97f349974451662fbfddc4
Mori <mori@felixjapan.net>
2025-08-04 14:33:10 +0900

    fix: 404ページ、絶対パスを変数に修正

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@ require get_stylesheet_directory() . '/include/my_variables.php';
 <?php get_header(); ?>
 <main class="err">
   <div class="wrap_s">
-    <h1 class="error_logo"><img src="https://pv.hanwha-japan.com/wp-content/uploads/2025/03/logo.png" alt="Hanwha Japan"></h1>
+    <h1 class="error_logo"><img src="<?= $home_url; ?>/wp-content/uploads/2025/03/logo.png" alt="Hanwha Japan"></h1>
     <!-- cont_err -->
     <div class="cont_err">
       <article class="err_ty">
@@ -14,7 +14,7 @@ require get_stylesheet_directory() . '/include/my_variables.php';
         <p class="txt">申し訳ございませんが、お探しのページが見つかりませんでした。お探しのページは削除されたか、URLが変更された可能性がございます。</p>
         <div class="btn_area">
           <!-- <a href="javascript:history.back();" class="btn_m btn_ty06">前ページ</a> -->
-          <a href="https://pv.hanwha-japan.com/about/" class="btn_m btn_ty02">メインページへ戻る</a>
+          <a href="<?= $home_url; ?>/about/" class="btn_m btn_ty02">メインページへ戻る</a>
           <!-- <a href="" class="btn_m btn_ty06">次ページ</a> -->
         </div>
       </article>
